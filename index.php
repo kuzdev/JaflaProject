4530d3671dbab0cc74a986f6ba339aca29320ed2
KuzDev <kuz.devlab@gmail.com>
2020-04-26 03:25:27 +0100

    jafla

---
+++ index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="style.css">
+
+
+</head>
+
+<body>
+
+  <div class="topnav" id="myTopnav">
+    <div class="container-fluid">
+      <div class="row">
+        <a href="#home" class="col-md-3 bg-red">Home</a>
+      </div>
+    </div>
+  </div>
+
+
+  <script>
+    function myFunction() {
+      var x = document.getElementById("myTopnav");
+      if (x.className === "topnav") {
+        x.className += " responsive";
+      } else {
+        x.className = "topnav";
+      }
+    }
+  </script>
+</body>
+
+</html>
---
+++ ini.php
@@ -0,0 +1,184 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+
+	<title>Document</title>
+
+	<style>
+		.carousel-control.right, .carousel-control.left {
+		background-image: none;
+		color: #f4511e;
+		}
+
+		.carousel-indicators li {
+			border-color: #f4511e;
+		}
+
+		.carousel-indicators li.active {
+			background-color: #f4511e;
+		}
+
+		.item h4 {
+			font-size: 19px;
+			line-height: 1.375em;
+			font-weight: 400;
+			font-style: italic;
+			margin: 70px 0;
+		}
+
+		.item span {
+			font-style: normal;
+		}
+	</style>
+</head>
+
+<body>
+
+<div class="container">
+<!-- <div class="bd-example">
+  <div id="carouselExampleCaptions" class="carousel slide" data-ride="carousel">
+    <ol class="carousel-indicators">
+      <li data-target="#carouselExampleCaptions" data-slide-to="0" class="active"></li>
+      <li data-target="#carouselExampleCaptions" data-slide-to="1"></li>
+      <li data-target="#carouselExampleCaptions" data-slide-to="2"></li>
+    </ol>
+    <div class="carousel-inner">
+      <div class="carousel-item active">
+        <img src="pic1.png" class="d-block w-100" alt="...">
+        <div class="carousel-caption d-none d-md-block">
+          <h5>First slide label</h5>
+          <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+        </div>
+      </div>
+      <div class="carousel-item">
+        <img src="pic1.png" class="d-block w-100" alt="...">
+        <div class="carousel-caption d-none d-md-block">
+          <h5>Second slide label</h5>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+        </div>
+      </div>
+      <div class="carousel-item">
+        <img src="pic1.png" class="d-block w-100" alt="...">
+        <div class="carousel-caption d-none d-md-block">
+          <h5>Third slide label</h5>
+          <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+        </div>
+      </div>
+    </div>
+    <a class="carousel-control-prev" href="#carouselExampleCaptions" role="button" data-slide="prev">
+      <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+      <span class="sr-only">Previous</span>
+    </a>
+    <a class="carousel-control-next" href="#carouselExampleCaptions" role="button" data-slide="next">
+      <span class="carousel-control-next-icon" aria-hidden="true"></span>
+      <span class="sr-only">Next</span>
+    </a>
+  </div>
+</div> -->
+
+<br><br>
+<div>
+<h1>Jafla Indonesia</h1>
+<p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nihil accusamus delectus perspiciatis, officia dolores vitae odit cum quam quas pariatur vero repudiandae suscipit tempore necessitatibus? Temporibus veritatis dicta enim possimus.</p>
+<button type="button" class="btn btn-danger btn-lg">Selengkapnya</button>
+</div>
+
+<br><br>
+<h1>Our Products</h1>
+	<div class="row">
+		<div class="col-md-4">
+			<div class="card" style="width: 18rem;">
+ 			 <img src="img/icon-fudd.png" class="card-img-top" alt="...">
+  				<div class="card-body">
+    				<h5 class="card-title">Jafla Food</h5>
+    				<p class="card-text">Menu Kuliner spesial pilihan dari Jafla</p>
+    				<a href="#" class="btn btn-danger">Buka Sekarang</a>
+  				</div>
+			</div>
+		</div>
+
+		<div class="col-md-4">
+			<div class="card" style="width: 18rem;">
+ 			 <img src="img/icon-butik.jpg" class="card-img-top" alt="...">
+  				<div class="card-body">
+    				<h5 class="card-title">Jafla Boutique</h5>
+    				<p class="card-text">Semua produk busana terbaik dari Jafla</p>
+    				<a href="#" class="btn btn-danger">Buka Sekarang</a>
+  				</div>
+			</div>
+		</div>
+
+		<div class="col-md-4">
+			<div class="card" style="width: 18rem;">
+ 			 <img src="img/icon-cam.jpg" class="card-img-top" alt="...">
+  				<div class="card-body">
+    				<h5 class="card-title">Jafla EO</h5>
+    				<p class="card-text">Jasa Event Organaizer andan dari Jafla</p>
+    				<a href="#" class="btn btn-danger">Buka Sekarang</a>
+  				</div>
+			</div>
+		</div>
+
+	</div>
+
+	<br><br><br>
+<div class="container">
+  <h2 class="text-center">What our customers say</h2>
+  <div id="myCarousel" class="carousel slide text-center" data-ride="carousel">
+    <!-- Indicators -->
+    <ol class="carousel-indicators">
+      <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+      <li data-target="#myCarousel" data-slide-to="1"></li>
+      <li data-target="#myCarousel" data-slide-to="2"></li>
+    </ol>
+
+    <!-- Wrapper for slides -->
+    <div class="carousel-inner" role="listbox">
+      <div class="item active">
+        <h4>"This company is the best. I am so happy with the result!"<br><span style="font-style:normal;">- Larry Poge</span></h4>
+      </div>
+      <div class="item">
+        <h4>"One word... AMAZING!!"<br><span style="font-style:normal;">- Bill Gatos</span></h4>
+      </div>
+      <div class="item">
+        <h4>"Could I... BE any more happy with this company?"<br><span style="font-style:normal;">Obuma</span></h4>
+      </div>
+    </div>
+
+    <!-- Left and right controls -->
+    <a class="left carousel-control" href="#myCarousel" role="button" data-slide="prev">
+      <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+      <span class="sr-only">Previous</span>
+    </a>
+    <a class="right carousel-control" href="#myCarousel" role="button" data-slide="next">
+      <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+      <span class="sr-only">Next</span>
+    </a>
+  </div>
+</div>
+</div>
+
+
+	<div class="container">
+	<h1>Our Location</h1>
+	<p>Jalan Ringroad Utara, Jombor, Mlati, Sleman, DIY</p>
+	<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d494.1771114044315!2d110.35588968293219!3d-7.745528108887794!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e7a596801cba945%3A0x8d6b95ac5764276!2sJAFLA+FOOD+INDONESIA!5e0!3m2!1sid!2sid!4v1564834210608!5m2!1sid!2sid" width="1065" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>
+	</div>
+
+</div>
+
+
+
+	<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+	<script src="https://code.jquery.com/jquery-2.2.4.min.js">
+</body>
+
+</html>
